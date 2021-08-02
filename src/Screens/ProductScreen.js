c30942483731fcd620c408c71a440a7f8ad3d467
dong uk <gusrn1255@naver.com>
2021-08-02 15:34:15 +0900

    add product detail screen

--- src/Screens/ProductScreen.js
+++ src/Screens/ProductScreen.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import products from "../Product";
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+
+    const product = products.find(p => p._id === match.params.id)
+    console.log(product)
+
+    console.log(match.params.id)
+
     return (
-        <div>
-            <h1>Product</h1>
-        </div>
+        <>
+            <Link to={"/"}>
+                Go Back
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image
+                        src={product.image}
+                        alt={product.name}
+                        fluid
+                    />
+                </Col>
+            </Row>
+        </>
     );
 };
 
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,35 +1,32 @@
 import React from 'react';
-import {Card} from 'react-bootstrap'
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import {Card} from "react-bootstrap";
 import Rating from "./Rating";
 
 const ProductCard = ({product}) => {
     return (
-        <Card className={'my-3 p-3 rounded'}>
-            <Link to={`/product/${product._id}`}>
-                <Card.Img src={product.image} variant={"top"}/>
-            </Link>
-            <Card.Body>
-                <Link to={`/product/${product._id}`}>
-                    <Card.Title as={"div"}>
+        <Link to={`/product/${product._id}`}>
+            <Card className={'my-3 p-3 rounded'}>
+                <Card.Img src={product.image} variant={"top"} />
+                <Card.Body>
+                    <Card.Title>
                         <strong>
                             {product.name}
                         </strong>
                     </Card.Title>
-                </Link>
-                <Card.Text as={"div"}>
-                    <Rating
-                        text={`${product.numReviews} reviews`}
-                        value={product.rating}
-                        color={"red"}
-                    />
-                </Card.Text>
-                <Card.Text as={"h3"}>
-                    $ {product.price}
-                </Card.Text>
-            </Card.Body>
-        </Card>
-
+                    <Card.Text as={"div"}>
+                        <Rating
+                            text={`${product.numReviews} reviews`}
+                            value={product.rating}
+                            color={"red"}
+                        />
+                    </Card.Text>
+                    <Card.Text as={"h3"}>
+                        $ {product.price}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Link>
     );
 };
 
